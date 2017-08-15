6f09f5b8831f48d0c384a7f70d8b959cf80d2765
edu-affiliates <mango@plasticjam.com>
2017-08-15 16:19:17 +0300

    substring service select CS

--- src/components/calculatorSmall/presentation/CSSelect.js
+++ src/components/calculatorSmall/presentation/CSSelect.js
@@ -11,6 +11,7 @@ class CalculatorSmallSelect extends React.Component {
 
 
     render() {
+        let service;
         let currentList = this.props.currentList.map(
             (item) => {
                 return <li key={item.id}
@@ -22,14 +23,17 @@ class CalculatorSmallSelect extends React.Component {
         );
         let searchService;
         if (this.props.type === 'service') {
-            searchService = <Search  calcId={this.props.calcId}/>;
+            searchService = <Search calcId={this.props.calcId}/>;
+            if (this.props.current.length > 15) {
+                service = this.props.current.substr(0, 15) + '...'
+            }
         }
         return (
 
             <div className="cs-select-wrap ">
                 <div onClick={() => this.props.toggleDropdown(this.props.type)}
                      className={`cs-select cs-select--${this.props.type}`}>
-                    {this.props.current}
+                    {(!!service) ? service : this.props.current}
                 </div>
                 <div className={(this.props.openDropdown[this.props.type]) ? 'open' : ''}>
                     <div className={`cs-dropdown-wrap cs-dropdown-wrap--${this.props.type}`}>
